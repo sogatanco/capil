90ff0ef928937f6cb8d60c1c909cffff8f3892b1
sogatanco <adeliapucha46@gmail.com>
2020-02-13 14:23:17 +0700

    1/2 welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,24 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{url('css/app.css')}}">
-
+    <link rel="stylesheet" href="{{url('css/welcome.css')}}">
     <script src="{{url('js/app.js')}}"></script>
 </head>
 <body>
+
+<!-- navbar -->
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+        <img src="{{url('images/logo.png')}}" alt="logo">
+    </a>
+  </div>
+</nav>
+
+<!-- jumbotron -->
+<div class="jumbotron" style="background-img">
+Kependudukan dan Pencatatan Sipil
+</div>
 
 </body>
 </html>
